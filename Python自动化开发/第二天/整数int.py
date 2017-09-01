b6523245bb76eaf00832aa2043b72349144e31b1
zhangyu <494167883@qq.com>
2017-09-01 17:49:51 +0800

    20170901

---
+++ Python自动化开发/第二天/整数int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+##################################【整数int】########################################
+# age = 8
+# print(age.bit_length())#当前整数的二进制表示，最少位数
+# age = 15
+# print(age.to_bytes(1,byteorder="big"))#获取当前数据的字节表示
