04487c3b9dc40cc2c80e7396c27a7f0d85547fce
zhangyu <494167883@qq.com>
2017-10-30 17:39:44 +0800

    20171030

---
+++ Python自动化开发/005/生成器.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# a = [1,2,3,4,5,6,7,8,9,10]
+# a = [i+1 for i in a]
+# print(a)
+
+# a = [1,2,3,4,5,6,7,8,9,10]
+# a = (i*i if i >5 else i for i in a)
+# print(a)
+# print(next(a))
+# print(next(a))
+# print(next(a))
+# print(next(a))
+
+
+
+
+
+
